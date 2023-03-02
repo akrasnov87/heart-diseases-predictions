b12fe8e47d52c42fda21ce820ec946918b1dd11e
Александр Краснов <vonsark87@yandex.ru>
2023-03-02 19:52:44 +0300

    добавлен интерфейс по тестированию модели

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from joblib import load
+
+st.title('Heart diseases prediction')
+
+st.text('Ниже требуется заполнить основные показатели для предсказания Вашего\nриска заболевания ССЗ')
+st.caption('Для корректного предсказания требуется сдать дополнительные анализа, а для этого лучше обратиться к Вашему лечащему врачу.')
+
+age = st.slider('Ваш возраст', 24, 65, 35)
+genre = st.radio('Ваш пол', ('Женский', 'Мужской'))
+if genre == 'Женский':
+    genre = 0
+else:
+    genre = 1
+height = st.number_input('Ваш рост', step=1, min_value=50, max_value=240, value=164)
+weight = st.number_input('Ваш вес', step=1, min_value=30, max_value=300, value=64)
+
+st.caption('Нормальное давление здорового человека 120 на 80')
+ap_hi = st.number_input('Ваш верхнее давление', step=1, min_value=60, max_value=260, value=120)
+ap_lo = st.number_input('Ваш нижнее давление', step=1, min_value=40, max_value=180, value=80)
+
+st.caption('Заполните информацию о Ваших анализах')
+cholesterol = st.selectbox('Ваш уровень холестерина',('1', '2', '3'))
+gluc = st.selectbox('Ваш уровень глюкозы',('1', '2', '3'))
+
+st.text('Осталось ответить ещё на пару вопросов:')
+
+is_smoke = st.checkbox('Вы курите?')
+is_alco = st.checkbox('Вы злоупотребляете алкоголем?')
+is_active = st.checkbox('Ведёте ли Вы активный образ жизни?')
+
+cls = load('heart.model')
+prediction = cls.predict_proba([[age, genre, height, weight, ap_hi, ap_lo, int(cholesterol), int(gluc), is_smoke, is_alco, is_active]])[:, 1]
+
+st.write(prediction)
